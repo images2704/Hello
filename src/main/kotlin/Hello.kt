83fb0314ae5c746ba3238229c2ced951f5e0eb0c
Stefan Aprilio <45485778+images2704@users.noreply.github.com>
2023-02-16 13:56:45 +0700

    Test

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,5 +1,15 @@
+import java.util.*
+fun feedTheFish(){
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
 fun main(args: Array<String>) {
-    val temperature = 10
-    val message = "The water temprature is ${if (temperature > 50) "too warm" else "ok"}"
-    println(message)
+    feedTheFish()
 }
